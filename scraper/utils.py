c678f34285377f8d332a369123ed899849ebac29
markbakos <markbakosss@gmail.com>
2025-04-16 14:54:30 +0200

    moved safe int convert to a global utils file and added safe float convert

---
+++ scraper/utils.py
@@ -0,0 +1,17 @@
+def safe_int_convert(text):
+    try:
+        text = text.strip()
+        if text == '--' or not text:
+            return 0
+        return int(text)
+    except (ValueError, TypeError):
+        return 0
+
+def safe_float_convert(text):
+    try:
+        text = text.strip()
+        if text == '--' or not text:
+            return 0
+        return float(text)
+    except (ValueError, TypeError):
+        return 0
