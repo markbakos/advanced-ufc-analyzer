d7be2e5cd0d077d1eb19c069a16e55748dc34cfb
markbakos <markbakosss@gmail.com>
2025-04-23 21:22:52 +0200

    reverted encoding categorical features being commented out

--- prediction/data_preprocessing.py
+++ prediction/data_preprocessing.py
@@ -389,7 +389,7 @@ class UFCDataPreprocessor:
         fights_df = self.calculate_days_since(fights_df)
         fights_df = self.handle_time_columns(fights_df)
         fights_df = self.engineer_features(fights_df)
-        # fights_df = self.encode_categorical(fights_df)
+        fights_df = self.encode_categorical(fights_df)
         fights_df = self.scale_features(fights_df)
         fights_df = self.remove_bias(fights_df)
 
