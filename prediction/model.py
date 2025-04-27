28a1fe80cd45e0d6bca5c3f82e77c5f377e0bbca
markbakos <markbakosss@gmail.com>
2025-04-27 14:40:39 +0200

    added kernel initializer and regularizers to the model

--- prediction/model.py
+++ prediction/model.py
@@ -12,21 +12,22 @@ def prediction_model(n_features, n_classes):
 
     masked_inputs = layers.Masking(mask_value=np.nan)(inputs)
 
-    # create a dense layer
-    x = layers.Dense(128, activation='relu')(masked_inputs)
+    # create dense layers
+    x = layers.Dense(128, activation='relu',  kernel_initializer='he_normal', kernel_regularizer=keras.regularizers.l2(0.001))(masked_inputs)
     x = layers.BatchNormalization()(x)
     x = layers.Dropout(0.3)(x)
 
-    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Dense(64, activation='relu', kernel_initializer='he_normal', kernel_regularizer=keras.regularizers.l2(0.001))(x)
     x = layers.BatchNormalization()(x)
     x = layers.Dropout(0.2)(x)
 
-    outputs = layers.Dense(n_classes, activation='softmax')(x)
+    outputs = layers.Dense(n_classes, activation='softmax', kernel_initializer='glorot_normal')(x)
 
     model = keras.Model(inputs=inputs, outputs=outputs)
 
     # compile model
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    optimizer = keras.optimizers.Adam(learning_rate=0.0005)
+    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     return model
 
