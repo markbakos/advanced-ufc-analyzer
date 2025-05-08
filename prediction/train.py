f702576041b3fa936278b2bb1d3095009deefbb2
markbakos <markbakosss@gmail.com>
2025-05-08 19:47:39 +0200

    moved model to base prediction directory

--- prediction/train.py
+++ prediction/train.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 import os
 from keras import callbacks
-from prediction.models.model import prediction_model
+from prediction.model import prediction_model
 from evaluate import evaluate_model
 
 def train_model():
