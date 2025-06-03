8a4b73fba1b212eea98984875297302e48e1a34f
markbakosss <markbakosss@gmail.com>
2025-06-03 16:17:49 +0200

    added refresh token and updated datetime now to use utcnow

--- server/api/auth/utils.py
+++ server/api/auth/utils.py
@@ -1,7 +1,7 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from server.core.config import settings
-from typing import Union, Any
+from typing import Union, Any, Optional
 from jose import jwt
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -14,13 +14,26 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verify if plain password matches hashed password"""
     return password_context.verify(plain_password, hashed_password)
 
-def create_access_token(data: Union[str, Any], expires_delta: int = None) -> str:
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     """Create JWT access token"""
+    to_encode = data.copy()
     if expires_delta is not None:
-        expires_delta = datetime.now(datetime.UTC) + expires_delta
+        expire = datetime.utcnow + expires_delta
     else:
-        expires_delta = datetime.now(datetime.UTC) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.utcnow + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
 
-    to_encode = {"exp": expires_delta, "sub": str(data)}
+    to_encode.update = {"exp": expire, "type": "access"}
     encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, settings.ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    """Create JWT refresh token"""
+    to_encode = data.copy()
+    if expires_delta is not None:
+        expire = datetime.utcnow + expires_delta
+    else:
+        expire = datetime.utcnow + timedelta(minutes=settings.REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update = {"exp": expire, "type": "access"}
+    encoded_jwt = jwt.encode(to_encode, settings.JWT_REFRESH_SECRET_KEY, settings.ALGORITHM)
     return encoded_jwt
