03071bac83f10679bb063b0835921966ee3dad05
markbakosss <markbakosss@gmail.com>
2025-06-03 22:18:11 +0200

    added users to routes

--- server/api/routes.py
+++ server/api/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from server.api.endpoints import predict
+from server.api.endpoints import predict, users
 
 router = APIRouter()
 
 router.include_router(predict.router, prefix="/predict", tags=["predict"])
+router.include_router(users.router, prefix="/users", tags=["users"])
