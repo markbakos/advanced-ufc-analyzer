db664c506a46c4739b20ddf37cf49d7ddb4279c5
markbakosss <markbakosss@gmail.com>
2025-06-03 19:47:12 +0200

    added database connection helper

---
+++ server/core/database.py
@@ -0,0 +1,27 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from typing import Optional
+from server.core.config import settings
+
+class Database:
+    client: Optional[AsyncIOMotorClient] = None
+    database = None
+
+db = Database()
+
+async def connect_to_mongo():
+    """COnnect to MongoDB"""
+    db.client = AsyncIOMotorClient(settings.MONGO_DB_URI)
+    db.database = db.client["ufc-prediction"]
+
+    await db.client.admin.command('ping')
+    print(f"Connected to MongoDB at {settings.MONGO_DB_URI}")
+
+async def close_mongo_connection():
+    """Close MongoDB connection"""
+    if db.client:
+        db.client.close()
+        print("Disconnected from MongoDB")
+
+def get_database():
+    """Get database instance"""
+    return db.database
