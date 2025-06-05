bf940fe2451fedf3702711d42877046cf8f60306
markbakosss <markbakosss@gmail.com>
2025-06-05 13:14:58 +0200

    updated bugs in auth utils

--- server/api/auth/utils.py
+++ server/api/auth/utils.py
@@ -18,11 +18,11 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -
     """Create JWT access token"""
     to_encode = data.copy()
     if expires_delta is not None:
-        expire = datetime.utcnow + expires_delta
+        expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
 
-    to_encode.update = {"exp": expire, "type": "access"}
+    to_encode.update({"exp": expire, "type": "access"})
     encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, settings.ALGORITHM)
     return encoded_jwt
 
@@ -30,10 +30,10 @@ def create_refresh_token(data: dict, expires_delta: Optional[timedelta] = None) 
     """Create JWT refresh token"""
     to_encode = data.copy()
     if expires_delta is not None:
-        expire = datetime.utcnow + expires_delta
+        expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow + timedelta(minutes=settings.REFRESH_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.utcnow() + timedelta(minutes=settings.REFRESH_TOKEN_EXPIRE_MINUTES)
 
-    to_encode.update = {"exp": expire, "type": "access"}
+    to_encode.update({"exp": expire, "type": "access"})
     encoded_jwt = jwt.encode(to_encode, settings.JWT_REFRESH_SECRET_KEY, settings.ALGORITHM)
     return encoded_jwt
